4dc08e90d7a8abe786658904d586342cf662a5f8
kevindryp <mateodryp@gmail.com>
2021-04-06 14:10:32 -0500

    Lab
    lab

---
+++ LaboratorioEstudiantes/LaboratorioEstudiantes-ejb/build/generated-sources/ap-source-output/co/edu/unipiloto/estudiante/entity/Estudiante_.java
@@ -0,0 +1,16 @@
+package co.edu.unipiloto.estudiante.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-06T13:58:48")
+@StaticMetamodel(Estudiante.class)
+public class Estudiante_ { 
+
+    public static volatile SingularAttribute<Estudiante, Integer> studentid;
+    public static volatile SingularAttribute<Estudiante, String> firstname;
+    public static volatile SingularAttribute<Estudiante, Integer> yearlevel;
+    public static volatile SingularAttribute<Estudiante, String> lastname;
+
+}
---
+++ LaboratorioEstudiantes/LaboratorioEstudiantes-ejb/src/java/co/edu/unipiloto/estudiante/session/EstudianteFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unipiloto.estudiante.session;
+
+import co.edu.unipiloto.estudiante.entity.Estudiante;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Usuario
+ */
+@Stateless
+public class EstudianteFacade extends AbstractFacade<Estudiante> implements EstudianteFacadeLocal {
+
+    @PersistenceContext(unitName = "EstudiantePU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EstudianteFacade() {
+        super(Estudiante.class);
+    }
+    
+}
